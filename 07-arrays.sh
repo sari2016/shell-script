8e1033d0515666d8d8ebc2b7a334b181d4dcfdeb
sari2016 <sarithagovindaraju@gmail.com>
2025-01-01 22:08:19 -0600

    info added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,8 +4,8 @@ MOVIES=("pushpa" "rrr" "devara")
 
 #index starts from 0, size is 3
 
-echo "First Movie: ${MOVIE[1]}"
-echo "Second Movie: ${MOVIE[2]}"
-echo "Third Movie: ${MOVIE[3]}"
+echo "First Movie: ${MOVIE[0]}"
+echo "Second Movie: ${MOVIE[1]}"
+echo "Third Movie: ${MOVIE[2]}"
 
 echo "All Movies: ${MOVIES[@]}"
